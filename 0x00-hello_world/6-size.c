36a4b5ec467c6a73514b046c39cea312668b33de
grufarik <austinegomilo7@gmail.com>
2023-02-16 18:31:46 -0800

    lion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  * Return: zero if error,non zero if no error
  */
 int main(void)
-{
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
+	printf("Size of a char: %ld bytes(s)\n", (unsigned long)sizeof(char));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(;long long int));
+	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
