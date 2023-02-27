6fe5ef6fa0a2e28a78a7546771730bdec78ad4ff
grufarik <austinegomilo7@gmail.com>
2023-02-27 01:05:28 -0800

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function to get the length of a string
+ * @s: string pointer to passed to this function
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
