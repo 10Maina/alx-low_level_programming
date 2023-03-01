23db616a816f0225758b2ed331567b9dfc29bff1
grufarik <austinegomilo7@gmail.com>
2023-03-01 11:41:38 -0800

    print_array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,15 @@
 
 void puts_half(char *str)
 {
-	int index = 0, len = 0, n;
+        int index = 0, len = 0, n;
 
-	while (str[index++])
-		len++;
-	if ((len % 2) == 0)
-		n = len / 2;
-	else
-		n = (len + 1) / 2;
-	for (index  = n; index < len; index++)
-		putchar(str[index]);
-	putchar('\n');
-}
+        while (str[index++])
+                len++;
+        if ((len % 2) == 0)
+                n = len / 2;
+        else
+                n = (len + 1) / 2;
+        for (index  = n; index < len; index++)
+                putchar(str[index]);
+        putchar('\n');
+}                           
